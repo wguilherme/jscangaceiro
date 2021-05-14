3b6ac1e71dec4e5ca565151d12037a2b50020ce7
W. Guilherme <ravok123@gmail.com>
2021-05-14 18:18:19 -0300

    [update] imutable class setup

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,18 +1,22 @@
 class Negociacao {
 
    constructor(data, quantidade, valor) {
-      this._data = data
+      this._data = new Date(data.getTime());
 
       this._quantidade = quantidade
 
       this._valor = valor
+
+      Object.freeze(this)
    }
 
-   getVolume() {
+   get volume() {
       return this._quantidade * this._valor
    }
 
-   getData() { return this._data }
-   getQuantidade() { return this._quantidade }
-   getValor() { return this._valor }
+   get data() {
+      return new Date(this._data.getTime())
+   }
+   get quantidade() { return this._quantidade }
+   get valor() { return this._valor }
 }
